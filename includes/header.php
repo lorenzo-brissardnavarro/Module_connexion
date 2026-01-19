e11bc94043e578f822231ac2739d7cdeb9cbfb26
lorenzo-brissardnavarro <lorenzo.brissardnavarro@laplateforme.io>
2026-01-19 10:17:27 +0100

    feat : creation des fichiers includes

---
+++ includes/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Module de connexion</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <a href="index.php">
+                <img src="" alt="">
+            </a>
+            <ul>
+                <li>
+                    <a href="">Inscription</a>
+                </li>
+                <li>
+                    <a href="">Connexion</a>
+                </li>
+                <li>
+                    <a href="">Modification</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+include 'includes/config.php';
+include 'includes/header.php';
+
+
+
+?>
+
+
+
+
+
+
+
+<?php include 'includes/footer.php'; ?>
